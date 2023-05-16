50810f6e8a42a70bec35298ff6d0398024190d3d
dodalsdl <132353354+dodalsdl@users.noreply.github.com>
2023-05-16 18:58:33 +0900

    Update Making_the_Zombie_Factory.sol

--- Making_the_Zombie_Factory.sol
+++ Making_the_Zombie_Factory.sol
@@ -26,7 +26,7 @@ contract ZombieFactory {
     function _generateRandomDna(string memory _str) private view returns (uint) { 
     //returns 뒤에 어떤 형태의 것이 반환되는지 알려줌
     //공개범위 뒤에 view 적음
-    /공개범위 제어자 returns(반환값)
+    //공개범위 제어자 returns(반환값)
     //외부 변화를 참조하지 않는 경우에는 pure를 작성함 - 이건 컴파일러에서 알려줌
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         //keccak함수는 일방향 함수여서 역산 불가능, 보안강화에 사용
